0afb25b53bd453926395801252335e1ee8c5b4dc
leonardo <elleoleonvalencia@gmail.com>
2019-10-03 17:19:48 -0400

    se listan los municipios en el navvarlinck desde la base de datos

--- src/components/Navbars/AdminNavbarLinks.js
+++ src/components/Navbars/AdminNavbarLinks.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -9,6 +9,9 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 // core components
 
 import styles from "assets/jss/material-dashboard-react/components/headerLinksStyle.js";
+import { async } from "q";
+
+import cubejs from '@cubejs-client/core';
 
 const useStyles = makeStyles(styles);
 const provincia = [
@@ -24,11 +27,45 @@ const municipio = [
   'Corralillo',
   'Caibarién',
 ];
+
+const API_URL = "http://192.168.0.10:4000"; // change to your actual endpoint
+
+const cubejsApi = cubejs(
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NjUxODE0NjMsImV4cCI6MTU2NTI2Nzg2M30.r3FYOTFyahrqGyE_BWF0HXeXlrDP8YDtWhWTRtehU0I",
+  { apiUrl: API_URL + "/cubejs-api/v1" }
+);
+
 export default function AdminNavbarLinks(props) {
   const classes = useStyles(props);
 
   const [provinciaName, setProvinciaName] = React.useState([]);
   const [municipioName, setMunicipioName] = React.useState([]);
+
+
+  const [municipioLista, setMunicipioLista] = React.useState([]);
+
+  useEffect(
+    async () => {
+    const tt = await cubejsApi.load({
+      "measures": [],
+      "timeDimensions": [],
+      "dimensions": [
+        "SymAgricUrbanaPoint.municipio"
+      ],
+      "filters": []
+    })
+    console.log(tt["loadResponse"]["data"])
+    var aux = []
+    tt["loadResponse"]["data"].map((p) =>
+      aux.push(p["SymAgricUrbanaPoint.municipio"])
+    )
+    console.log(aux)
+
+    setMunicipioLista(aux);
+    console.log(municipioLista)
+  },
+    [props]
+  )
 
   const handleChangeP = event => {
     setProvinciaName(event.target.value);
@@ -81,7 +118,7 @@ export default function AdminNavbarLinks(props) {
           <MenuItem value='' disabled className={classes.dropdownItem}>
             <LocationOnIcon className={classes.icons} style={{ fontSize: 16 }} /> Municipio
           </MenuItem>
-          {municipio.map(name => (
+          {municipioLista.map(name => (
             <MenuItem key={name} value={name} className={classes.dropdownItem}>
               {name}
             </MenuItem>
